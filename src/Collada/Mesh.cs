33ada5513262cef90e273f39887778760e3f260f
David Srbecký <dsrbecky@gmail.com>
2006-09-25 13:01:53 +0000

    Render multiple Polylists; Use Input's Semantic

--- src/Collada/Mesh.cs
+++ src/Collada/Mesh.cs
@@ -1,8 +1,3 @@
-// <file>
-//     <copyright name="David Srbecký" email="dsrbecky@gmail.com"/>
-//     <license name="GPL"/>
-// </file>
-
 using System;
 using System.Collections.Generic;
 using System.Xml.Serialization;
@@ -15,30 +10,10 @@ namespace Collada
 	{
 		public void Render()
 		{
-			float[] position = (this.Source[0].Item as FloatArray).Floats;
-			float[] normals  = (this.Source[1].Item as FloatArray).Floats;
-			int[] vcount = (this.Items[0] as Polylist).VcountArray;
-			int[] p = (this.Items[0] as Polylist).PArray;
-			
-			float scale = 1/200f;
-			
-			int pIndex = 0;
-			for(int polygon = 0; polygon < vcount.Length; polygon++) {
-				Gl.glBegin(Gl.GL_POLYGON);
-				{
-					for(int vertex = 0; vertex < vcount[polygon]; vertex++) {
-						Gl.glColor3f(1.0f - 0.2f * vertex, 1.0f - 0.2f * vertex, 1.0f - 0.2f * vertex);
-						int posIndex = p[pIndex++] * 3;
-						Gl.glVertex3d(position[posIndex] * scale,
-						              position[posIndex+1] * scale,
-						              position[posIndex+2] * scale);
-						int nomIndex = p[pIndex++] * 3;
-						Gl.glNormal3d(normals[nomIndex],
-						              normals[nomIndex+1],
-						              normals[nomIndex+2]);
-					}
-				} 
-				Gl.glEnd();
+			foreach(object item in this.Items) {
+				if (item is Polylist) {
+					((Polylist)item).Render();
+				}
 			}
 		}
 	}
--- src/Collada/Polylist.cs
+++ src/Collada/Polylist.cs
@@ -1,8 +1,3 @@
-// <file>
-//     <copyright name="David Srbecký" email="dsrbecky@gmail.com"/>
-//     <license name="GPL"/>
-// </file>
-
 using System;
 using System.Collections.Generic;
 using System.Xml.Serialization;
@@ -17,12 +12,12 @@ namespace Collada
 		///described by the polylist element. The vcount element may occur once.
 		///</summary>
 		[XmlIgnore]
-		public int[] VcountArray {
+		public ulong[] VcountArray {
 			get {
 				string[] txts = this.Vcount.Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries);
-				int[] ret = new int[txts.Length];
+				ulong[] ret = new ulong[txts.Length];
 				for(int i = 0; i < txts.Length; i++) {
-					ret[i] = int.Parse(txts[i]);
+					ret[i] = ulong.Parse(txts[i]);
 				}
 				return ret;
 			}
@@ -31,14 +26,73 @@ namespace Collada
 		/// <summary>The p element may occur once.
 		///</summary>
 		[XmlIgnore]
-		public int[] PArray {
+		public ulong[] PArray {
 			get {
 				string[] txts = this.P.Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries);
-				int[] ret = new int[txts.Length];
+				ulong[] ret = new ulong[txts.Length];
 				for(int i = 0; i < txts.Length; i++) {
-					ret[i] = int.Parse(txts[i]);
+					ret[i] = ulong.Parse(txts[i]);
 				}
 				return ret;
+			}
+		}
+		
+		public void Render()
+		{
+			ulong  pStride = 0;
+			ulong  positionOffset = 0;
+			Source positionSource = null;
+			ulong  normalOffset = 0;
+			Source normalSource = null;
+			foreach(InputLocalOffset input in this.Input) {
+				pStride = Math.Max(pStride, input.Offset + 1);
+				if (input.Semantic == "VERTEX") {
+					Vertices vertices = Vertices.IDs[input.Source.Remove(0,1)];
+					foreach(InputLocal vertexInput in vertices.Input) {
+						if (vertexInput.Semantic == "POSITION") {
+							positionOffset = input.Offset;
+							positionSource = Source.IDs[vertexInput.Source.Remove(0,1)];
+						}
+						if (vertexInput.Semantic == "NORMAL") {
+							normalOffset = input.Offset;
+							normalSource = Source.IDs[vertexInput.Source.Remove(0,1)];
+						}
+					}
+				}
+				if (input.Semantic == "POSITION") {
+					positionOffset = input.Offset;
+					positionSource = Source.IDs[input.Source.Remove(0,1)];
+				}
+				if (input.Semantic == "NORMAL") {
+					normalOffset = input.Offset;
+					normalSource = Source.IDs[input.Source.Remove(0,1)];
+				}
+			}
+			float[] positions = (positionSource.Item as FloatArray).Floats;
+			float[] normals   = (normalSource.Item as FloatArray).Floats;
+			
+			
+			ulong[] vcounts = this.VcountArray;
+			ulong[] p = this.PArray;
+			
+			double scale = 200;
+			
+			ulong pIndex = 0;
+			foreach(ulong vcount in vcounts) {
+				Gl.glBegin(Gl.GL_POLYGON); {
+					for(ulong vertex = 0; vertex < vcount; vertex++) {
+						Gl.glColor3f(1.0f - 0.2f * vertex, 1.0f - 0.2f * vertex, 1.0f - 0.2f * vertex);
+						ulong posIndex = p[pIndex + positionOffset] * 3;
+						Gl.glVertex3d(positions[posIndex] / scale,
+						              positions[posIndex+1] / scale,
+						              positions[posIndex+2] / scale);
+						ulong nomIndex = p[pIndex + normalOffset] * 3;
+						Gl.glNormal3d(normals[nomIndex],
+						              normals[nomIndex+1],
+						              normals[nomIndex+2]);
+						pIndex += pStride;
+					}
+				} Gl.glEnd();
 			}
 		}
 	}
